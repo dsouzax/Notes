e5e5c2ceb9b8d2febf99882bb22843aead9407b1
Xavier Dsouza <xavidsouza54@gmail.com>
2023-07-13 19:27:44 -0400

    Added notes json and used props to populate notes

---
+++ src/Components/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(notesItem =>  
+      <Note
+        key = {notesItem.key}
+        title = {notesItem.title}
+        content = {notesItem.content} />
+      )}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./Components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
